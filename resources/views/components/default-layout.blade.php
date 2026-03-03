0b2c9a5dc1a1adff9b49b86de3a5b08ac3e0ea12
DYLANeay <dylan.eray6@gmail.com>
2026-03-03 11:23:26 +0100

    création des vues des différentes pages

--- resources/views/components/default-layout.blade.php
+++ resources/views/components/default-layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     @isset($title)
-        <title>{{ $title }} - {{ config('app.name') }}</title>
+        <title>{{ $title }} | {{ config('app.name') }}</title>
     @else
         <title>{{ config('app.name') }}</title>
     @endisset
