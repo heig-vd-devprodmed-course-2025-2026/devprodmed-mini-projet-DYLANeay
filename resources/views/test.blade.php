1fcaf68a1295509ce2b0f5c0834d11cbc1ddd836
DYLANeay <dylan.eray6@gmail.com>
2026-03-03 11:00:07 +0100

    Create and add styling to the default layout

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+{{-- Donc ici le but étant que l'élément HTML prenne le nom du component! (pour que ça marche) --}}
+<x-default-layout>
+    {{--     <x-slot:title>
+        Test de layout 2
+    </x-slot:title> --}}
+    <h1>Ceci est une vue de test</h1>
+
+    <p>
+        Si vous voyez ce message, cela signifie que le layout commun fonctionne
+        correctement !
+    </p>
+</x-default-layout>
