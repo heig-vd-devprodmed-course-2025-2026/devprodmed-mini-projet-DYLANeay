8a37c5f36e212a9b8cf2a1746a389a9441c4aecb
DYLANeay <dylan.eray6@gmail.com>
2026-03-10 11:18:34 +0100

    update the homepage to link to the page with all posts

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->get();
-
-    return view('home', ['posts' => $posts]);
-});
 
 Route::get('/about', function () {
     return view('about');
@@ -17,4 +12,11 @@ Route::get('/about', function () {
 
 
 Route::get('/@{username}', [ProfileController::class, 'show'])->where('username', '[A-Za-z0-9-_]+');
+
 Route::resource('posts', PostController::class);
+
+Route::get('/', function () {
+    $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->limit(3)->get();
+
+    return view('home', ['posts' => $posts]);
+});
