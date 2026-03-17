c7a47c22037dea4afa91e93a14abaabf10c242b6
DYLANeay <dylan.eray6@gmail.com>
2026-03-17 09:59:29 +0100

    setup post form, modify and delete a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,60 +4,103 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\User;
 
-class PostController extends Controller {
+class PostController extends Controller
+{
     /**
      * Display a listing of the resource.
      */
-    public function index() {
-        $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->get();
+    public function index()
+    {
+        $posts = Post::orderBy("created_at", "desc")
+            ->with("user")
+            ->with("likes")
+            ->get();
 
-        return view('posts.index', ['posts' => $posts]);
+        return view("posts.index", ["posts" => $posts]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {
-        return view('posts.create');
+    public function create()
+    {
+        return view("posts.create");
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage. (post added in this case)
      */
-    public function store(Request $request) {
-        //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            "title" => "nullable|string|max:255",
+            "content" => "required|string|min:10|max:5000",
+        ]);
+
+        $user = User::where("id", 2)->first();
+        $post = new Post();
+
+        $post->title = $validated["title"];
+        $post->content = $validated["content"];
+        $post->user()->associate($user);
+
+        $post->save();
+
+        return redirect("/posts/$post->id");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id) {
-        $post = Post::with('user')->with('likes')->findOrFail($id);
+    public function show(string $id)
+    {
+        $post = Post::with("user")->with("likes")->findOrFail($id);
 
-        return view('posts.show', ['post' => $post]);
+        return view("posts.show", ["post" => $post]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id) {
+    public function edit(string $id)
+    {
         $post = Post::findOrFail($id);
 
-        return view('posts.edit', ['post' => $post]);
+        return view("posts.edit", ["post" => $post]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id) {
-        //
+    /**
+     * Update the specified resource in storage. (Updating a post in this case)
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            "title" => "nullable|string|max:255",
+            "content" => "required|string|max:5000",
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        $post->title = $validated["title"];
+        $post->content = $validated["content"];
+
+        $post->save();
+
+        return redirect("/posts/$post->id");
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource from storage. (here, deleting a post)
      */
-    public function destroy(string $id) {
-        //
+    public function destroy(string $id)
+    {
+        Post::destroy($id);
+
+        return redirect("/posts");
     }
 }
