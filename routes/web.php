f5b62a8b8cb27ac413ef94f2b15ddc889efcd808
DYLANeay <dylan.eray6@gmail.com>
2026-03-24 15:20:19 +0100

    auth middleware to avoid unauth people to access some pages

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,10 @@ Route::get("/about", function () {
 Route::controller(AuthController::class)->group(function () {
     Route::get("/auth/register", "showRegister");
     Route::post("/auth/register", "register");
-    Route::get("/auth/login", "showLogin");
+    //name permet de nommer la route, ce qui est utile pour générer des URLs dans les vues (et rediriger voir plus bas)
+    Route::get("/auth/login", "showLogin")->name("login");
     Route::post("/auth/login", "login");
-    Route::post("/auth/logout", "logout");
+    Route::post("/auth/logout", "logout")->middleware("auth");
 });
 
 Route::get("/@{username}", [ProfileController::class, "show"])->where(
@@ -26,13 +27,22 @@ Route::get("/@{username}", [ProfileController::class, "show"])->where(
     "[A-Za-z0-9-_]+",
 );
 
-Route::resource("posts", PostController::class);
+Route::resource("posts", PostController::class)
+    ->except(["index", "show"])
+    //->only(["create", "store", "edit", "update", "destroy"])
+    // middleware s'applique à tous par défaut
+    // si l'user n'est pas connecté, il sera redirigé vers la page de login (voir la route de login plus haut)
+    ->middleware("auth");
+
+Route::resource("posts", PostController::class)->only(["index", "show"]);
 
 Route::get("/", [HomeController::class, "index"]);
 
 Route::match(["put", "patch"], "/likes/{post}", [
     LikeController::class,
     "update",
-]);
+])->middleware("auth");
 
-Route::singleton("my-profile", MyProfileController::class)->destroyable();
+Route::singleton("my-profile", MyProfileController::class)
+    ->destroyable()
+    ->middleware("auth");
