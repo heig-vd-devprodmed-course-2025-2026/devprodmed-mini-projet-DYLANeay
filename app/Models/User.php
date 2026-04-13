947a1768d087fc3d94de2446171198af8fc4ef21
DYLANeay <dylan.eray6@gmail.com>
2026-04-13 09:17:26 +0200

    sanctum, migration, token controller & config

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
+    use HasApiTokens;
     /**
      * Get the posts for the user.
      */
