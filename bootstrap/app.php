8d87ac17c0521c83dbbfe961a2d261731c82ab31
DYLANeay <dylan.eray6@gmail.com>
2026-04-24 13:40:00 +0200

    add admin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "abilities" => CheckAbilities::class,
             "ability" => CheckForAnyAbility::class,
+            "admin" => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
